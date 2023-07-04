5634f73d82bb20ea07f5da23a2b5fd124ea051e1
Boris <betttttt@yandex.ru>
2023-07-04 16:48:15 +0300

    The real

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Employee sam = new Employee("Sam", "Brunov", 20, "Yandex", 25);
 //        Boss bob = new Boss("Boris", "Yakunin", 15, "Microsoft");
 //        bob.fireEmployee(sam);
-        Person p = new Boss("Boris", "Yakunin", 15, "Microsoft");
-        System.out.println(p.name);
+//        Person p = new Boss("Boris", "Yakunin", 15, "Microsoft");
+//        System.out.println(p.name);
     }
 }
